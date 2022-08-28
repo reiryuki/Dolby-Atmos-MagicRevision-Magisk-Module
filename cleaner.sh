5573af59572d4d2db4f7de76f9f537d70e9eec8a
Riki Irawan <lightningthunder9999@gmail.com>
2022-08-28 11:53:58 +0000

    Upload New File

---
+++ cleaner.sh
@@ -0,0 +1,12 @@
+MODPATH=${0%/*}
+APP="`ls $MODPATH/system/priv-app` `ls $MODPATH/system/app`"
+for APPS in $APP; do
+  rm -f `find /data/system/package_cache -type f -name *$APPS*`
+  rm -f `find /data/dalvik-cache /data/resource-cache -type f -name *$APPS*.apk`
+done
+PKG=com.atmos
+for PKGS in $PKG; do
+  rm -rf /data/user/*/$PKGS/cache/*
+done
+
+

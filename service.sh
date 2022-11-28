bcdb94c1ba4a8f03c76ed6162f38d0992e75ce56
Riki Irawan <lightningthunder9999@gmail.com>
2022-11-28 08:55:55 +0000

    Replace service.sh

--- service.sh
+++ service.sh
@@ -18,6 +18,20 @@ if [ -d $DIR ] && [ ! -f $AML/disable ] && [ "$API" -ge 26 ]; then
   chcon -R u:object_r:vendor_configs_file:s0 $DIR
 fi
 
+# magisk
+if [ -d /sbin/.magisk ]; then
+  MAGISKTMP=/sbin/.magisk
+else
+  MAGISKTMP=`realpath /dev/*/.magisk`
+fi
+
+# path
+MIRROR=$MAGISKTMP/mirror
+SYSTEM=`realpath $MIRROR/system`
+VENDOR=`realpath $MIRROR/vendor`
+ODM=`realpath $MIRROR/odm`
+MY_PRODUCT=`realpath $MIRROR/my_product`
+
 # mount
 NAME="*audio*effects*.conf -o -name *audio*effects*.xml -o -name *policy*.conf -o -name *policy*.xml"
 if [ -d $AML ] && [ ! -f $AML/disable ]\
@@ -29,7 +43,8 @@ else
   DIR=$MODPATH/system/vendor
 fi
 FILE=`find $DIR/etc -maxdepth 1 -type f -name $NAME`
-if [ "`realpath /odm/etc`" == /odm/etc ] && [ "$FILE" ]; then
+if [ ! -d $ODM ] && [ "`realpath /odm/etc`" == /odm/etc ]\
+&& [ "$FILE" ]; then
   for i in $FILE; do
     j="/odm$(echo $i | sed "s|$DIR||")"
     if [ -f $j ]; then
@@ -38,7 +53,8 @@ if [ "`realpath /odm/etc`" == /odm/etc ] && [ "$FILE" ]; then
     fi
   done
 fi
-if [ -d /my_product/etc ] && [ "$FILE" ]; then
+if [ ! -d $MY_PRODUCT ] && [ -d /my_product/etc ]\
+&& [ "$FILE" ]; then
   for i in $FILE; do
     j="/my_product$(echo $i | sed "s|$DIR||")"
     if [ -f $j ]; then

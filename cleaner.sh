81b3eb533e4c035d94f1a878a89c07caf4bd2c41
Riki Irawan <lightningthunder9999@gmail.com>
2023-01-16 08:40:36 +0000

    Replace cleaner.sh

--- cleaner.sh
+++ cleaner.sh
@@ -4,7 +4,7 @@ for APPS in $APP; do
   rm -f `find /data/system/package_cache -type f -name *$APPS*`
   rm -f `find /data/dalvik-cache /data/resource-cache -type f -name *$APPS*.apk`
 done
-PKG=com.atmos
+PKG=`cat $MODPATH/package.txt`
 for PKGS in $PKG; do
   rm -rf /data/user/*/$PKGS/cache/*
 done

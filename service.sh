7423a640dc93b231a1b07d14d3de29cf2a5a963b
Riki Irawan <lightningthunder9999@gmail.com>
2023-06-04 16:04:19 +0000

    Replace service.sh

--- service.sh
+++ service.sh
@@ -1,6 +1,5 @@
 MODPATH=${0%/*}
 API=`getprop ro.build.version.sdk`
-AML=/data/adb/modules/aml
 
 # debug
 exec 2>$MODPATH/debug.log
@@ -25,56 +24,47 @@ killall android.hardware.sensors@1.0-service
 sleep 20
 
 # aml fix
-DIR=$AML/system/vendor/odm/etc
-if [ "$API" -ge 26 ]\
-&& [ -d $DIR ] && [ ! -f $AML/disable ]; then
+AML=/data/adb/modules/aml
+if [ -L $AML/system/vendor ]\
+&& [ -d $AML/vendor ]; then
+  DIR=$AML/vendor/odm/etc
+else
+  DIR=$AML/system/vendor/odm/etc
+fi
+if [ "$API" -ge 26 ] && [ -d $DIR ]\
+&& [ ! -f $AML/disable ]; then
   chcon -R u:object_r:vendor_configs_file:s0 $DIR
 fi
-
-# magisk
-if [ -d /sbin/.magisk ]; then
-  MAGISKTMP=/sbin/.magisk
+AUD=`grep AUD= $MODPATH/copy.sh | sed -e 's|AUD=||g' -e 's|"||g'`
+if [ -L $AML/system/vendor ]\
+&& [ -d $AML/vendor ]; then
+  DIR=$AML/vendor
 else
-  MAGISKTMP=`realpath /dev/*/.magisk`
-fi
-
-# path
-MIRROR=$MAGISKTMP/mirror
-SYSTEM=`realpath $MIRROR/system`
-VENDOR=`realpath $MIRROR/vendor`
-ODM=`realpath $MIRROR/odm`
-MY_PRODUCT=`realpath $MIRROR/my_product`
-
-# mount
-NAME="*audio*effects*.conf -o -name *audio*effects*.xml -o -name *policy*.conf -o -name *policy*.xml"
-if [ -d $AML ] && [ ! -f $AML/disable ]\
-&& find $AML/system/vendor -type f -name $NAME; then
-  NAME="*audio*effects*.conf -o -name *audio*effects*.xml"
-#p  NAME="*audio*effects*.conf -o -name *audio*effects*.xml -o -name *policy*.conf -o -name *policy*.xml"
   DIR=$AML/system/vendor
-else
-  DIR=$MODPATH/system/vendor
 fi
-FILES=`find $DIR/etc -maxdepth 1 -type f -name $NAME`
-if [ ! -d $ODM ] && [ "`realpath /odm/etc`" == /odm/etc ]\
-&& [ "$FILES" ]; then
-  for FILE in $FILES; do
-    DES="/odm$(echo $FILE | sed "s|$DIR||")"
-    if [ -f $DES ]; then
-      umount $DES
-      mount -o bind $FILE $DES
-    fi
-  done
-fi
-if [ ! -d $MY_PRODUCT ] && [ -d /my_product/etc ]\
-&& [ "$FILES" ]; then
-  for FILE in $FILES; do
-    DES="/my_product$(echo $FILE | sed "s|$DIR||")"
-    if [ -f $DES ]; then
-      umount $DES
-      mount -o bind $FILE $DES
-    fi
-  done
+FILES=`find $DIR -type f -name $AUD`
+if [ -d $AML ] && [ ! -f $AML/disable ]\
+&& find $DIR -type f -name $AUD; then
+  if ! grep '/odm' $AML/post-fs-data.sh && [ -d /odm ]\
+  && [ "`realpath /odm/etc`" == /odm/etc ]; then
+    for FILE in $FILES; do
+      DES=/odm`echo $FILE | sed "s|$DIR||g"`
+      if [ -f $DES ]; then
+        umount $DES
+        mount -o bind $FILE $DES
+      fi
+    done
+  fi
+  if ! grep '/my_product' $AML/post-fs-data.sh\
+  && [ -d /my_product ]; then
+    for FILE in $FILES; do
+      DES=/my_product`echo $FILE | sed "s|$DIR||g"`
+      if [ -f $DES ]; then
+        umount $DES
+        mount -o bind $FILE $DES
+      fi
+    done
+  fi
 fi
 
 # wait
@@ -86,6 +76,11 @@ done
 PKG=com.atmos
 if [ "$API" -ge 30 ]; then
   appops set $PKG AUTO_REVOKE_PERMISSIONS_IF_UNUSED ignore
+fi
+PKGOPS=`appops get $PKG`
+UID=`dumpsys package $PKG 2>/dev/null | grep -m 1 userId= | sed 's/    userId=//'`
+if [ "$UID" -gt 9999 ]; then
+  UIDOPS=`appops get --uid "$UID"`
 fi
 
 # function
@@ -103,7 +98,7 @@ if [ "$NEXTPID" ]; then
 else
   PID=`pidof $SERVER`
 fi
-sleep 10
+sleep 15
 stop_log
 NEXTPID=`pidof $SERVER`
 if [ "`getprop init.svc.$SERVER`" != stopped ]; then

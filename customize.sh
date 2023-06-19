bc65d334ca35eeb2eb44816c2835f769bf543b7e
Rei Ryuki the Fixer <lightningthunder9999@gmail.com>
2023-06-19 22:57:36 +0000

    Replace customize.sh

--- customize.sh
+++ customize.sh
@@ -1,6 +1,17 @@
 # space
 ui_print " "
 
+# log
+if [ "$BOOTMODE" != true ]; then
+  FILE=/sdcard/$MODID\_recovery.log
+  ui_print "- Log will be saved at $FILE"
+  exec 2>$FILE
+  ui_print " "
+fi
+
+# run
+. $MODPATH/function.sh
+
 # info
 MODVER=`grep_prop version $MODPATH/module.prop`
 MODVERCODE=`grep_prop versionCode $MODPATH/module.prop`
@@ -11,146 +22,12 @@ if [ "$KSU" == true ]; then
   ui_print " KSUVersion=$KSU_VER"
   ui_print " KSUVersionCode=$KSU_VER_CODE"
   ui_print " KSUKernelVersionCode=$KSU_KERNEL_VER_CODE"
+  sed -i 's|#k||g' $MODPATH/post-fs-data.sh
 else
   ui_print " MagiskVersion=$MAGISK_VER"
   ui_print " MagiskVersionCode=$MAGISK_VER_CODE"
 fi
 ui_print " "
-
-# huskydg function
-get_device() {
-PAR="$1"
-DEV="`cat /proc/self/mountinfo | awk '{ if ( $5 == "'$PAR'" ) print $3 }' | head -1 | sed 's/:/ /g'`"
-}
-mount_mirror() {
-SRC="$1"
-DES="$2"
-RAN="`head -c6 /dev/urandom | base64 | tr -dc 'a-zA-Z0-9'`"
-while [ -e /dev/$RAN ]; do
-  RAN="`head -c6 /dev/urandom | base64 | tr -dc 'a-zA-Z0-9'`"
-done
-mknod /dev/$RAN b `get_device "$SRC"; echo $DEV`
-if mount -t ext4 -o ro /dev/$RAN "$DES"\
-|| mount -t erofs -o ro /dev/$RAN "$DES"\
-|| mount -t f2fs -o ro /dev/$RAN "$DES"\
-|| mount -t ubifs -o ro /dev/$RAN "$DES"; then
-  blockdev --setrw /dev/$RAN
-  rm -f /dev/$RAN
-  return 0
-fi
-rm -f /dev/$RAN
-return 1
-}
-unmount_mirror() {
-DIRS="$MIRROR/system_root $MIRROR/system $MIRROR/vendor
-      $MIRROR/product $MIRROR/system_ext $MIRROR/odm
-      $MIRROR/my_product $MIRROR"
-for DIR in $DIRS; do
-  umount $DIR
-done
-}
-mount_odm_to_mirror() {
-DIR=/odm
-if [ -d $DIR ]; then
-  ui_print "- Mount $MIRROR$DIR..."
-  mkdir -p $MIRROR$DIR
-  if mount_mirror $DIR $MIRROR$DIR; then
-    ui_print "  $MIRROR$DIR mount success"
-  else
-    ui_print "  ! $MIRROR$DIR mount failed"
-    rm -rf $MIRROR$DIR
-    if [ -d $MIRROR/system_root$DIR ]; then
-      ln -sf $MIRROR/system_root$DIR $MIRROR
-    fi
-  fi
-  ui_print " "
-fi
-}
-mount_my_product_to_mirror() {
-DIR=/my_product
-if [ -d $DIR ]; then
-  ui_print "- Mount $MIRROR$DIR..."
-  mkdir -p $MIRROR$DIR
-  if mount_mirror $DIR $MIRROR$DIR; then
-    ui_print "  $MIRROR$DIR mount success"
-  else
-    ui_print "  ! $MIRROR$DIR mount failed"
-    rm -rf $MIRROR$DIR
-    if [ -d $MIRROR/system_root$DIR ]; then
-      ln -sf $MIRROR/system_root$DIR $MIRROR
-    fi
-  fi
-  ui_print " "
-fi
-}
-mount_partitions_to_mirror() {
-unmount_mirror
-# mount system
-if [ "$SYSTEM_ROOT" == true ]; then
-  DIR=/system_root
-  ui_print "- Mount $MIRROR$DIR..."
-  mkdir -p $MIRROR$DIR
-  if mount_mirror / $MIRROR$DIR; then
-    ui_print "  $MIRROR$DIR mount success"
-    rm -rf $MIRROR/system
-    ln -sf $MIRROR$DIR/system $MIRROR
-  else
-    ui_print "  ! $MIRROR$DIR mount failed"
-    rm -rf $MIRROR$DIR
-  fi
-else
-  DIR=/system
-  ui_print "- Mount $MIRROR$DIR..."
-  mkdir -p $MIRROR$DIR
-  if mount_mirror $DIR $MIRROR$DIR; then
-    ui_print "  $MIRROR$DIR mount success"
-  else
-    ui_print "  ! $MIRROR$DIR mount failed"
-    rm -rf $MIRROR$DIR
-  fi
-fi
-ui_print " "
-# mount vendor
-DIR=/vendor
-ui_print "- Mount $MIRROR$DIR..."
-mkdir -p $MIRROR$DIR
-if mount_mirror $DIR $MIRROR$DIR; then
-  ui_print "  $MIRROR$DIR mount success"
-else
-  ui_print "  ! $MIRROR$DIR mount failed"
-  rm -rf $MIRROR$DIR
-  ln -sf $MIRROR/system$DIR $MIRROR
-fi
-ui_print " "
-# mount product
-DIR=/product
-ui_print "- Mount $MIRROR$DIR..."
-mkdir -p $MIRROR$DIR
-if mount_mirror $DIR $MIRROR$DIR; then
-  ui_print "  $MIRROR$DIR mount success"
-else
-  ui_print "  ! $MIRROR$DIR mount failed"
-  rm -rf $MIRROR$DIR
-  ln -sf $MIRROR/system$DIR $MIRROR
-fi
-ui_print " "
-# mount system_ext
-DIR=/system_ext
-ui_print "- Mount $MIRROR$DIR..."
-mkdir -p $MIRROR$DIR
-if mount_mirror $DIR $MIRROR$DIR; then
-  ui_print "  $MIRROR$DIR mount success"
-else
-  ui_print "  ! $MIRROR$DIR mount failed"
-  rm -rf $MIRROR$DIR
-  if [ -d $MIRROR/system$DIR ]; then
-    ln -sf $MIRROR/system$DIR $MIRROR
-  fi
-fi
-ui_print " "
-mount_odm_to_mirror
-mount_my_product_to_mirror
-}
 
 # sdk
 NUM=17
@@ -164,20 +41,11 @@ else
   ui_print " "
 fi
 
+# recovery
+mount_partitions_in_recovery
+
 # magisk
-MAGISKPATH=`magisk --path`
-if [ "$BOOTMODE" == true ]; then
-  if [ "$MAGISKPATH" ]; then
-    mount -o rw,remount $MAGISKPATH
-    MAGISKTMP=$MAGISKPATH/.magisk
-    MIRROR=$MAGISKTMP/mirror
-  else
-    MAGISKTMP=/mnt
-    mount -o rw,remount $MAGISKTMP
-    MIRROR=$MAGISKTMP/mirror
-    mount_partitions_to_mirror
-  fi
-fi
+magisk_setup
 
 # path
 SYSTEM=`realpath $MIRROR/system`
@@ -199,17 +67,6 @@ MY_PRODUCT=`realpath $MIRROR/my_product`
 OPTIONALS=/sdcard/optionals.prop
 if [ ! -f $OPTIONALS ]; then
   touch $OPTIONALS
-fi
-
-# mount
-if [ "$BOOTMODE" != true ]; then
-  if [ -e /dev/block/bootdevice/by-name/vendor ]; then
-    mount -o rw -t auto /dev/block/bootdevice/by-name/vendor /vendor
-  else
-    mount -o rw -t auto /dev/block/bootdevice/by-name/cust /vendor
-  fi
-  mount -o rw -t auto /dev/block/bootdevice/by-name/persist /persist
-  mount -o rw -t auto /dev/block/bootdevice/by-name/metadata /metadata
 fi
 
 # sepolicy
@@ -249,11 +106,7 @@ if [ "$BOOTMODE" == true ]; then
     RES=`pm uninstall $PKGS 2>/dev/null`
   done
 fi
-rm -rf /metadata/magisk/$MODID
-rm -rf /mnt/vendor/persist/magisk/$MODID
-rm -rf /persist/magisk/$MODID
-rm -rf /data/unencrypted/magisk/$MODID
-rm -rf /cache/magisk/$MODID
+remove_sepolicy_rule
 ui_print " "
 
 # function
@@ -277,6 +130,7 @@ for NAMES in $NAME; do
   rm -rf /persist/magisk/$NAMES
   rm -rf /data/unencrypted/magisk/$NAMES
   rm -rf /cache/magisk/$NAMES
+  rm -rf /cust/magisk/$NAMES
 done
 }
 
@@ -308,29 +162,28 @@ fi
 
 # function
 permissive_2() {
-sed -i '1i\
-SELINUX=`getenforce`\
-if [ "$SELINUX" == Enforcing ]; then\
-  magiskpolicy --live "permissive *"\
-fi\' $MODPATH/post-fs-data.sh
+sed -i 's|#2||g' $MODPATH/post-fs-data.sh
 }
 permissive() {
-SELINUX=`getenforce`
-if [ "$SELINUX" == Enforcing ]; then
-  setenforce 0
-  SELINUX=`getenforce`
-  if [ "$SELINUX" == Enforcing ]; then
+FILE=/sys/fs/selinux/enforce
+SELINUX=`cat $FILE`
+if [ "$SELINUX" == 1 ]; then
+  if ! setenforce 0; then
+    echo 0 > $FILE
+  fi
+  SELINUX=`cat $FILE`
+  if [ "$SELINUX" == 1 ]; then
     ui_print "  Your device can't be turned to Permissive state."
     ui_print "  Using Magisk Permissive mode instead."
     permissive_2
   else
-    setenforce 1
-    sed -i '1i\
-SELINUX=`getenforce`\
-if [ "$SELINUX" == Enforcing ]; then\
-  setenforce 0\
-fi\' $MODPATH/post-fs-data.sh
+    if ! setenforce 1; then
+      echo 1 > $FILE
+    fi
+    sed -i 's|#1||g' $MODPATH/post-fs-data.sh
   fi
+else
+  sed -i 's|#1||g' $MODPATH/post-fs-data.sh
 fi
 }
 
@@ -408,8 +261,8 @@ FILE=$MODPATH/.aml.sh
 PROP=`grep_prop stream.mode $OPTIONALS`
 if echo "$PROP" | grep -q m; then
   ui_print "- Activating music stream..."
-  sed -i 's/#m//g' $FILE
-  sed -i 's/musicstream=/musicstream=true/g' $MODPATH/acdb.conf
+  sed -i 's|#m||g' $FILE
+  sed -i 's|musicstream=|musicstream=true|g' $MODPATH/acdb.conf
   ui_print " "
 else
   APP=AudioFX
@@ -419,27 +272,72 @@ else
 fi
 if echo "$PROP" | grep -q r; then
   ui_print "- Activating ring stream..."
-  sed -i 's/#r//g' $FILE
+  sed -i 's|#r||g' $FILE
   ui_print " "
 fi
 if echo "$PROP" | grep -q a; then
   ui_print "- Activating alarm stream..."
-  sed -i 's/#a//g' $FILE
+  sed -i 's|#a||g' $FILE
   ui_print " "
 fi
 if echo "$PROP" | grep -q s; then
   ui_print "- Activating system stream..."
-  sed -i 's/#s//g' $FILE
+  sed -i 's|#s||g' $FILE
   ui_print " "
 fi
 if echo "$PROP" | grep -q v; then
   ui_print "- Activating voice_call stream..."
-  sed -i 's/#v//g' $FILE
+  sed -i 's|#v||g' $FILE
   ui_print " "
 fi
 if echo "$PROP" | grep -q n; then
   ui_print "- Activating notification stream..."
-  sed -i 's/#n//g' $FILE
+  sed -i 's|#n||g' $FILE
+  ui_print " "
+fi
+if echo "$PROP" | grep -q b; then
+  ui_print "- Activating bluetooth_sco stream..."
+  sed -i 's|#b||g' $FILE
+  ui_print " "
+fi
+if echo "$PROP" | grep -q f; then
+  ui_print "- Activating dtmf stream..."
+  sed -i 's|#f||g' $FILE
+  ui_print " "
+fi
+if echo "$PROP" | grep -q e; then
+  ui_print "- Activating enforced_audible stream..."
+  sed -i 's|#e||g' $FILE
+  ui_print " "
+fi
+if echo "$PROP" | grep -q y; then
+  ui_print "- Activating accessibility stream..."
+  sed -i 's|#y||g' $FILE
+  ui_print " "
+fi
+if echo "$PROP" | grep -q t; then
+  ui_print "- Activating tts stream..."
+  sed -i 's|#t||g' $FILE
+  ui_print " "
+fi
+if echo "$PROP" | grep -q i; then
+  ui_print "- Activating assistant stream..."
+  sed -i 's|#i||g' $FILE
+  ui_print " "
+fi
+if echo "$PROP" | grep -q c; then
+  ui_print "- Activating call_assistant stream..."
+  sed -i 's|#c||g' $FILE
+  ui_print " "
+fi
+if echo "$PROP" | grep -q p; then
+  ui_print "- Activating patch stream..."
+  sed -i 's|#p||g' $FILE
+  ui_print " "
+fi
+if echo "$PROP" | grep -q g; then
+  ui_print "- Activating rerouting stream..."
+  sed -i 's|#g||g' $FILE
   ui_print " "
 fi
 
